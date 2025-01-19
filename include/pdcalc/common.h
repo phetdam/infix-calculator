64760bbd23d6cfd1bd273ba78af5eb91d22026c7
Derek Huang <djh458@stern.nyu.edu>
2025-01-18 23:54:47 -0500

    common.h: add PDCALC_CPLUSPLUS for correct __cplusplus macro

--- include/pdcalc/common.h
+++ include/pdcalc/common.h
@@ -21,4 +21,11 @@
 #define PDCALC_STRINGIFY_I(x) #x
 #define PDCALC_STRINGIFY(x) PDCALC_STRINGIFY_I(x)
 
+// C++ version macro that works with MSVC (avoid using /Zc:__cplusplus)
+#if defined(_MSVC_LANG)
+#define PDCALC_CPLUSPLUS _MSVC_LANG
+#elif defined(__cplusplus)
+#define PDCALC_CPLUSPLUS __cplusplus
+#endif  // !defined(_MSVC_LANG) && !defined(__cplusplus)
+
 #endif  // PDCALC_COMMON_H_
